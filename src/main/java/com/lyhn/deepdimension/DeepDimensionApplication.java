865b2902844725fc596c66cf3b2abb24ae97458e
JavaLyHn <2443560753@qq.com>
2026-01-27 02:07:45 +0800

    DeepDimension初始化创建

---
+++ src/main/java/com/lyhn/deepdimension/DeepDimensionApplication.java
@@ -0,0 +1,13 @@
+package com.lyhn.deepdimension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDimensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepDimensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyhn/deepdimension/DeepDimensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyhn.deepdimension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDimensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
